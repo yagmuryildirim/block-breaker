71f8ff46d287b90007851b12b51a79742a73f381
yagmuryildirim <yagmurcyildirim@gmail.com>
2021-09-04 01:11:56 +0300

    level1 done1 git add .

--- Block Breaker 2/Assets/Scripts/Ball.cs
+++ Block Breaker 2/Assets/Scripts/Ball.cs
@@ -42,4 +42,10 @@ public class Ball : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (hasStarted)
+            GetComponent<AudioSource>().Play();
+    }
 }
